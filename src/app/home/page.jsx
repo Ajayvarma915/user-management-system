c7ef7754c24694d08b859d74e380a60ae740c012
unknown <kammampatiajayvarma@gmail.com>
2024-08-23 21:43:55 +0530

    path modified

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,5 +1,5 @@
 import { auth } from "@/auth";
-import Options from "@/components/options";
+import Options from "../../components/Options";
 import { redirect } from "next/navigation";
 export default async function Home() {
 
